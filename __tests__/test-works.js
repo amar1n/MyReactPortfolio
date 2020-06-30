65884a95decefbbdccd03fb58841e15d6dcc9c8e
Alberto Marín <alberto.marin@gmail.com>
2020-06-30 12:11:00 +0200

    Card text adjustment

--- __tests__/test-works.js
+++ __tests__/test-works.js
@@ -1,9 +1,8 @@
 'use strict';
 
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Works from '../src/components/works';
-import CardWork from '../src/components/work_card';
 
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -12,15 +11,17 @@ Enzyme.configure({ adapter: new Adapter() });
 import efimerum from '../src/assets/images/work/efimerum.png';
 
 const myWork = [
-    {
+    {   
         name: 'Efimerum\'s backend',
-        info: 'EFIMERUM is an App that allows you to share photos without ornaments in an ephemeral way, where the life of the photo is not decided by the user who publishes it, but rather by the community through its "Likes". The Backend was developed with Express.js, Firebase and Google Cloud Platform (Vision, Storage, Compute Engine).',
+        info: ' is the heart of EFIMERUM, the App that allows you to share photos without ornaments in an ephemeral way, where the life of the photo is not decided by the user who publishes it, but rather by the community through its "Likes".\n\nDeveloped in 2017 using Express.js, Firebase and Google Cloud Platform (Vision, Storage, Compute Engine).',
         img: efimerum,
-        alt: 'Contact List',
+        alt: 'Efimerum',
         codeLink: 'https://github.com/amar1n/efimerum-backend',
         demoLink: '',
         youtubeLink: 'https://youtu.be/S3Vpv01AwRc',
-        imgLarge: ''
+        imgLarge: '',
+        appleStore: '',
+        googlePlay: ''
     },
 ];
 
